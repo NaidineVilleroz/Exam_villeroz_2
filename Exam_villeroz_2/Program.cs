79d22114ee51d8cdf8f8514daa19da1182b4c2bf
naidine <villeroznaidine19@gmail.com>
2023-05-04 18:34:33 +0800

    Add project files.

---
+++ Exam_villeroz_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // StreamReader example
+        Console.WriteLine("=== StreamReader Example ===");
+        using (StreamReader reader = new StreamReader("C:\\Users\\ville\\Downloads\\examplestream.txt"))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        // StreamWriter example
+        Console.WriteLine("=== StreamWriter Example ===");
+        using (StreamWriter writer = new StreamWriter("C:\\Users\\ville\\Downloads\\log.txt", true))
+        {
+            string message = "This is my StreamWriter logging details " + DateTime.Now.ToString() + ".";
+            writer.WriteLine(message);
+            Console.WriteLine(message);
+        }
+    }
+}
